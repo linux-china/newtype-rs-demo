cbe5d7c982e5a803fb859423a7a9a38e3b05188b
Libing Chen <libing.chen@gmail.com>
2024-06-19 23:01:17 +0800

    app: models

---
+++ src/model.rs
@@ -0,0 +1,83 @@
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+use nutype::nutype;
+use regex::Regex;
+
+lazy_static! {
+    static ref EMAIL_REGEX: Regex = Regex::new(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").unwrap();
+    static ref UUID_REGEX: Regex = Regex::new("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$").unwrap();
+}
+
+#[nutype(
+    sanitize(trim, lowercase),
+    validate(
+        len_char_min = 5,
+        len_char_max = 20,
+        regex = EMAIL_REGEX,
+    ),
+    derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash),
+)]
+pub struct EmailAddress(String);
+
+#[nutype(
+    sanitize(trim),
+    validate(
+        len_char_min = 36,
+        len_char_max = 36,
+        regex = UUID_REGEX,
+    ),
+    derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash),
+)]
+pub struct UUID(String);
+
+#[nutype(
+    sanitize(trim),
+    validate(
+        len_char_min = 6,
+        len_char_max = 12,
+    ),
+    derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash),
+)]
+pub struct Password(String);
+
+#[nutype(
+    validate(
+        greater_or_equal = 16,
+        less_or_equal = 150
+    ),
+    derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash),
+)]
+pub struct Age(u16);
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct User {
+    pub id: u64,
+    pub uuid: UUID,
+    pub email: EmailAddress,
+    pub password: Password,
+    pub age: Option<Age>,
+}
+
+impl User {
+    pub fn new(id: u64, uuid: &str, email: &str, password: &str, age: u16) -> anyhow::Result<Self> {
+        let user = User {
+            id,
+            uuid: UUID::new(uuid.to_string()).unwrap(),
+            email: EmailAddress::new(email.to_string()).unwrap(),
+            password: Password::new(password.to_string()).unwrap(),
+            age: Age::new(age).ok(),
+        };
+        Ok(user)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_user() {
+        let user = User::new(1, "a50c4df7-6c08-428d-ad5a-258a2fadfed7", "demo@example.com", "$ww6%234", 36).unwrap();
+        println!("{}", serde_json::to_string(&user).unwrap());
+    }
+}
