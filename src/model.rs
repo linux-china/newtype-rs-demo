3654685ccafd9b55e5419162567b0426e2822116
Libing Chen <libing.chen@gmail.com>
2025-02-03 11:51:53 +0800

    chore: API adjusted

--- src/model.rs
+++ src/model.rs
@@ -1,11 +1,13 @@
 use lazy_static::lazy_static;
-use serde::{Deserialize, Serialize};
 use nutype::nutype;
 use regex::Regex;
+use serde::{Deserialize, Serialize};
 
 lazy_static! {
-    static ref EMAIL_REGEX: Regex = Regex::new(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").unwrap();
-    static ref UUID_REGEX: Regex = Regex::new("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$").unwrap();
+    static ref EMAIL_REGEX: Regex =
+        Regex::new(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").unwrap();
+    static ref UUID_REGEX: Regex =
+        Regex::new("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$").unwrap();
 }
 
 #[nutype(
@@ -28,7 +30,7 @@ lazy_static! {
         PartialOrd,
         Ord,
         Hash
-    ),
+    )
 )]
 pub struct EmailAddress(String);
 
@@ -39,26 +41,50 @@ pub struct EmailAddress(String);
         len_char_max = 36,
         regex = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$",
     ),
-    derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash),
+    derive(
+        Serialize,
+        Deserialize,
+        Debug,
+        Clone,
+        PartialEq,
+        Eq,
+        PartialOrd,
+        Ord,
+        Hash
+    )
 )]
 pub struct UUID(String);
 
 #[nutype(
     sanitize(trim),
-    validate(
-        len_char_min = 6,
-        len_char_max = 12,
-    ),
-    derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash),
+    validate(len_char_min = 6, len_char_max = 12,),
+    derive(
+        Serialize,
+        Deserialize,
+        Debug,
+        Clone,
+        PartialEq,
+        Eq,
+        PartialOrd,
+        Ord,
+        Hash
+    )
 )]
 pub struct Password(String);
 
 #[nutype(
-    validate(
-        greater_or_equal = 16,
-        less_or_equal = 150
-    ),
-    derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash),
+    validate(greater_or_equal = 16, less_or_equal = 150),
+    derive(
+        Serialize,
+        Deserialize,
+        Debug,
+        Clone,
+        PartialEq,
+        Eq,
+        PartialOrd,
+        Ord,
+        Hash
+    )
 )]
 pub struct Age(u16);
 
@@ -75,10 +101,10 @@ impl User {
     pub fn new(id: u64, uuid: &str, email: &str, password: &str, age: u16) -> anyhow::Result<Self> {
         let user = User {
             id,
-            uuid: UUID::new(uuid.to_string()).unwrap(),
-            email: EmailAddress::new(email.to_string()).unwrap(),
-            password: Password::new(password.to_string()).unwrap(),
-            age: Age::new(age).ok(),
+            uuid: UUID::try_new(uuid.to_string())?,
+            email: EmailAddress::try_new(email.to_string())?,
+            password: Password::try_new(password.to_string())?,
+            age: Age::try_new(age).ok(),
         };
         Ok(user)
     }
@@ -90,20 +116,27 @@ mod tests {
 
     #[test]
     fn test_age() {
-        let user_age = Age::new(32).unwrap();
+        let user_age = Age::try_new(32).unwrap();
         println!("{:?}", user_age)
     }
 
     #[test]
     fn test_email() {
-        let email = EmailAddress::new("deMo@example.com").unwrap();
+        let email = EmailAddress::try_new("deMo@example.com").unwrap();
         println!("{}", email);
         println!("uppercase: {}", email.to_uppercase());
     }
 
     #[test]
     fn test_user() {
-        let user = User::new(1, "a50c4df7-6c08-428d-ad5a-258a2fadfed7", "demo@example.com", "$ww6%234", 36).unwrap();
+        let user = User::new(
+            1,
+            "a50c4df7-6c08-428d-ad5a-258a2fadfed7",
+            "demo@example.com",
+            "$ww6%234",
+            36,
+        )
+        .unwrap();
         println!("{}", serde_json::to_string(&user).unwrap());
     }
 }
